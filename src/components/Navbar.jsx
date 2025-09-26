d294e7aa01374689ffd164885850d984f83140db
Alyssa <xcthaeh@gmail.com>
2025-09-26 00:44:09 -0400

    Added comments to NavBar for hand off

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,18 @@ export default function Navbar() {
 
   return (
     <nav className="navbar">
+    {/* Alyssa: Just make sure routes and count work */}
       <Link to="/" className="brand">EZTechMovie</Link>
       <div className="nav-links">
         <NavLink to="/subscriptions">Subscriptions</NavLink>
         <NavLink to="/accessories">Accessories</NavLink>
         <NavLink to="/cart">Cart{count > 0 ? ` (${count})` : ''}</NavLink>
       </div>
+
+      {/* Weston: Improve this later 
+          - Add better styling, cart badge UI
+          - Add toast/alert integration for warnings
+          - Empty state polish */}
     </nav>
   );
 }
